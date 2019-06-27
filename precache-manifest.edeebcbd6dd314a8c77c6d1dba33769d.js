90bbd09b514dc9bd5f75e0a225f0981c868fe53c
Tobibur Rahman <tobiburrahman786@gmail.com>
2019-06-27 23:57:17 +0530

    Updates

---
+++ precache-manifest.edeebcbd6dd314a8c77c6d1dba33769d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95bfe593d5ea3468098d0ae5b6762c64",
+    "url": "/my-notes-app/index.html"
+  },
+  {
+    "revision": "f01e14a7325dde31bb66",
+    "url": "/my-notes-app/static/css/main.43e54023.chunk.css"
+  },
+  {
+    "revision": "9fc7fb0b020ad718388a",
+    "url": "/my-notes-app/static/js/2.15d407c9.chunk.js"
+  },
+  {
+    "revision": "f01e14a7325dde31bb66",
+    "url": "/my-notes-app/static/js/main.6590de90.chunk.js"
+  },
+  {
+    "revision": "0ab4fac4399957826a4a",
+    "url": "/my-notes-app/static/js/runtime~main.b8b0b4f2.js"
+  },
+  {
+    "revision": "7ee640f70ac495811737bcb55afa7ef5",
+    "url": "/my-notes-app/static/media/arrow-back.7ee640f7.svg"
+  },
+  {
+    "revision": "2a17b27cfb41ca358997c6461cc29fd8",
+    "url": "/my-notes-app/static/media/cancel.2a17b27c.svg"
+  },
+  {
+    "revision": "99b8715675515663f02992179bf62c78",
+    "url": "/my-notes-app/static/media/notes-add.99b87156.svg"
+  },
+  {
+    "revision": "cd8e7c84b8aaf8c0742db5981e3c4807",
+    "url": "/my-notes-app/static/media/person.cd8e7c84.svg"
+  },
+  {
+    "revision": "cb707987bca64ce2ad4f4db57323ad8c",
+    "url": "/my-notes-app/static/media/search.cb707987.svg"
+  }
+]);
